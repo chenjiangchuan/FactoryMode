7b1e3b645a675b9bc669f681b809e96f62ec7998
jc <jiangchuanc@gmail.com>
2017-03-30 16:44:51 +0800

    Initial Commit

---
+++ 工厂方法/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  工厂方法
+//
+//  Created by chenjiangchuan on 2017/3/30.
+//  Copyright © 2017年 chenjiangchuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
